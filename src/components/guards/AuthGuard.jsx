3fc0baff93c780c2183e9766053cc2a0b58f3a9a
SBinakin <sundaybinakin@gmail.com>
2024-11-15 14:10:14 +0000

    guard fixed

--- src/components/guards/AuthGuard.jsx
+++ src/components/guards/AuthGuard.jsx
@@ -1,18 +1,29 @@
+// import { Navigate } from 'react-router-dom';
+
+// const AuthGuard = ({ children, allowedRole }) => {
+//     const token = localStorage.getItem('token');
+//     const userRole = localStorage.getItem('role');
+
+//     if (!token) {
+//         return <Navigate to="/login" replace />;
+//     }
+
+//     if (allowedRole && userRole !== allowedRole) {
+//         return <Navigate to="/" replace />;
+//     }
+
+//     return children;
+// };
+
+// export default AuthGuard; 
+
+import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-const AuthGuard = ({ children, allowedRole }) => {
-    const token = localStorage.getItem('token');
-    const userRole = localStorage.getItem('role');
+const AuthGuard = ({ children }) => {
+    const isAuthenticated = !!localStorage.getItem("token"); // Check for token in local storage
 
-    if (!token) {
-        return <Navigate to="/login" replace />;
-    }
-
-    if (allowedRole && userRole !== allowedRole) {
-        return <Navigate to="/" replace />;
-    }
-
-    return children;
+    return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
-export default AuthGuard; 
+export default AuthGuard;
